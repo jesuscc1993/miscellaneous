ba21bfe4e97e5d21c1469bf41ce41b8bbfdd7256
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2021-05-04 20:55:58 +0200

    Added makeshift unit viewer

---
+++ gbf-tools/units-viewer/index.js
@@ -0,0 +1,32 @@
+const fetchJson = (url) => {
+  return fetch(url).then((response) => response.json());
+};
+
+const initialize = () => {
+  fetchJson('units.json').then((elements) => {
+    const output = jQuery('#output');
+
+    Object.keys(elements).forEach((element) => {
+      const units = elements[element];
+
+      const unitsGrid = jQuery(`<div class="units-grid"></div>`);
+
+      units.forEach((unit) => {
+        unitsGrid.append(
+          `<img class="unit-portrait" src="http://game-a1.granbluefantasy.jp/assets_en/img/sp/assets/npc/m/${unit}_01.jpg">`
+        );
+      });
+
+      const elementUnits = jQuery(
+        `<div class="element ${element}">
+          <h3 id="${element}">${element.toUpperCase()}</h3>
+        </div>`
+      );
+
+      elementUnits.append(unitsGrid);
+      output.append(elementUnits);
+    });
+  });
+};
+
+initialize();
