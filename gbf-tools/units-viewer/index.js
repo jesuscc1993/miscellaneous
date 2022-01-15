b58a87d98c674efc905c5855f5704bec291623c2
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2022-01-15 18:06:08 +0100

    Updated GBF character viewer.

--- gbf-tools/units-viewer/index.js
+++ gbf-tools/units-viewer/index.js
@@ -1,13 +1,50 @@
+/*
+  units replacement regex    .*\/m\/(.*?)_.*
+*/
+
 const fetchJson = (url) => {
   return fetch(url).then((response) => response.json());
 };
 
-const initialize = () => {
-  fetchJson('units.json').then((elements) => {
-    const output = jQuery('#output');
+const getBaseUrl = () => {
+  return location.protocol + '//' + location.host + location.pathname;
+};
 
-    Object.keys(elements).forEach((element) => {
-      const units = elements[element];
+const getSearchParams = () => {
+  if (!location.search.length) return [];
+
+  const params = location.search.slice(1).split('&');
+  return params.reduce((acc, param) => {
+    const [key, value] = param.split('=');
+    acc[key] = value;
+    return acc;
+  }, {});
+};
+
+const initialize = () => {
+  const searchParams = getSearchParams();
+  const elementParam = searchParams.element;
+
+  const output = jQuery('#output');
+
+  fetchJson('units.json').then((elementsMap) => {
+    const elementKeys = Object.keys(elementsMap);
+
+    if (elementParam) {
+      output.append(`
+        <div class="links-container">
+          <h3>Elements</h3>
+          <a href="${getBaseUrl()}">🡠 All elements</a>
+        </div>
+      `);
+    } else {
+      generateElementLinks(elementKeys, elementParam);
+    }
+
+    elementKeys.forEach((element) => {
+      if (elementParam && elementParam !== element) return;
+
+      const units = elementsMap[element];
 
       const unitsGrid = jQuery(`<div class="units-grid"></div>`);
 
@@ -27,6 +64,37 @@ const initialize = () => {
       output.append(elementUnits);
     });
   });
+};
+
+const generateElementLinks = (elements) => {
+  const output = jQuery('#output');
+
+  const baseUrl = getBaseUrl();
+
+  const elementAnchors = jQuery(
+    '<div class="links-container"><h3>Elements</h3></div>'
+  );
+
+  const sectionAnchors = jQuery(
+    '<div class="links-container"><h3>Sections</h3></div>'
+  );
+
+  elements.forEach((element, i) => {
+    const label = element.toUpperCase();
+
+    if (i > 0) {
+      elementAnchors.append(`/`);
+      sectionAnchors.append(`/`);
+    }
+
+    elementAnchors.append(
+      `<a href="${baseUrl}?element=${element}">${label}</a>`
+    );
+    sectionAnchors.append(`<a href="${baseUrl}#${element}">${label}</a>`);
+  });
+
+  output.append(elementAnchors);
+  output.append(sectionAnchors);
 };
 
 initialize();
