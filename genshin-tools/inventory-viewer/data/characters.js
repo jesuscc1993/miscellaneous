a40329f2f5d09413313cc901a152700a8a01586c
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-02-07 21:03:06 +0100

    Added Yelan.

--- genshin-tools/inventory-viewer/data/characters.js
+++ genshin-tools/inventory-viewer/data/characters.js
@@ -1,9 +1,187 @@
 const characters = [
   {
+    id: 'Aloy',
+    level: 50,
+    rarity: 4.5,
+  },
+  {
+    id: 'Ambor',
+    level: 50,
+    name: 'Amber',
+    rarity: 4,
+    uncap: 4,
+  },
+  {
     id: 'Ayaka',
     level: 90,
     name: 'Ayaka',
-    rarity: 5
+    rarity: 5,
+  },
+  {
+    id: 'Barbara',
+    level: 80,
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'Beidou',
+    level: 80,
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'Bennett',
+    level: 80,
+    rarity: 4,
+    uncap: 4,
+  },
+  {
+    id: 'Chongyun',
+    level: 80,
+    rarity: 4,
+    uncap: 4,
+  },
+  {
+    id: 'Collei',
+    level: 50,
+    rarity: 4,
+    uncap: 1,
+  },
+  {
+    id: 'Diluc',
+    level: 90,
+    rarity: 5,
+    uncap: 2,
+  },
+  {
+    id: 'Diona',
+    level: 80,
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'Dori',
+    level: 50,
+    rarity: 4,
+    uncap: 1,
+  },
+  {
+    id: 'Eula',
+    level: 90,
+    rarity: 5,
+  },
+  {
+    id: 'Feiyan',
+    level: 80,
+    name: 'Yanfei',
+    rarity: 4,
+    uncap: 5,
+  },
+  {
+    id: 'Fischl',
+    level: 80,
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'Ganyu',
+    level: 90,
+    rarity: 5,
+  },
+  {
+    id: 'Gorou',
+    level: 50,
+    rarity: 4,
+  },
+  {
+    id: 'Heizo',
+    level: 50,
+    name: 'Heizou',
+    rarity: 4,
+  },
+  {
+    id: 'Kaeya',
+    level: 50,
+    rarity: 4,
+    uncap: 1,
+  },
+  {
+    fullName: 'Kaedehara Kazuha',
+    id: 'Kazuha',
+    level: 80,
+    rarity: 5,
+  },
+  {
+    id: 'Keqing',
+    level: 80,
+    rarity: 5,
+  },
+  {
+    id: 'Lisa',
+    level: 50,
+    rarity: 4,
+    uncap: 2,
+  },
+  {
+    id: 'Mona',
+    level: 80,
+    rarity: 5,
+  },
+  {
+    id: 'Ningguang',
+    level: 80,
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'Noel',
+    level: 80,
+    name: 'Noelle',
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'PlayerGirl',
+    level: 80,
+    name: 'Tabibito',
+    rarity: 5,
+    uncap: 6,
+  },
+  {
+    id: 'Qin',
+    level: 80,
+    name: 'Jean',
+    rarity: 5,
+  },
+  {
+    id: 'Qiqi',
+    level: 80,
+    rarity: 5,
+  },
+  {
+    id: 'Razor',
+    level: 50,
+    rarity: 4,
+    uncap: 6,
+  },
+  {
+    id: 'Rosaria',
+    level: 80,
+    rarity: 4,
+    uncap: 2,
+  },
+  {
+    id: 'Sara',
+    level: 80,
+    name: 'Kujou Sara',
+    rarity: 4,
+    uncap: 2,
+  },
+  {
+    id: 'Sayu',
+    level: 50,
+    rarity: 4,
+    uncap: 5,
   },
   {
     fullName: 'Raiden Shogun',
@@ -13,232 +191,59 @@ const characters = [
     rarity: 5,
   },
   {
-    id: 'Diluc',
-    level: 90,
-    rarity: 5,
-    uncap: 2
-  },
-  {
-    id: 'Eula',
-    level: 90,
-    rarity: 5
-  },
-  {
-    id: 'Ganyu',
-    level: 90,
-    rarity: 5
-  },
-  {
-    id: 'Qin',
-    level: 80,
-    name: 'Jean',
-    rarity: 5
-  },
-  {
-    id: 'PlayerGirl',
-    level: 80,
-    name: 'Tabibito',
-    rarity: 5,
-    uncap: 6
+    id: 'Sucrose',
+    level: 60,
+    rarity: 4,
+    uncap: 4,
   },
   {
     id: 'Tartaglia',
     level: 80,
-    rarity: 5
-  },
-  {
-    fullName: 'Kaedehara Kazuha',
-    id: 'Kazuha',
-    level: 80,
-    rarity: 5
-  },
-  {
-    id: 'Keqing',
-    level: 80,
-    rarity: 5
-  },
-  {
-    id: 'Mona',
-    level: 80,
-    rarity: 5
-  },
-  {
-    id: 'Qiqi',
-    level: 80,
-    rarity: 5
-  },
-  {
-    id: 'Venti',
-    level: 80,
-    rarity: 5
-  },
-  {
-    id: 'Aloy',
-    level: 50,
-    rarity: 4.5
-  },
-  {
-    id: 'Sara',
-    level: 80,
-    name: 'Kujou Sara',
-    rarity: 4,
-    uncap: 2
-  },
-  {
-    id: 'Noel',
-    level: 80,
-    name: 'Noelle',
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Barbara',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Beidou',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Bennett',
-    level: 80,
-    rarity: 4,
-    uncap: 4
-  },
-  {
-    id: 'Chongyun',
-    level: 80,
-    rarity: 4,
-    uncap: 4
-  },
-  {
-    id: 'Diona',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Fischl',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Ningguang',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Rosaria',
-    level: 80,
-    rarity: 4,
-    uncap: 2
-  },
-  {
-    id: 'Xiangling',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Xingqiu',
-    level: 80,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Feiyan',
-    level: 80,
-    name: 'Yanfei',
-    rarity: 4,
-    uncap: 5
-  },
-  {
-    id: 'Sucrose',
-    level: 60,
-    rarity: 4,
-    uncap: 4
-  },
-  {
-    id: 'Ambor',
-    level: 50,
-    name: 'Amber',
-    rarity: 4,
-    uncap: 4
-  },
-  {
-    id: 'Heizo',
-    level: 50,
-    name: 'Heizou',
-    rarity: 4
+    rarity: 5,
   },
   {
     id: 'Tohma',
     level: 50,
     name: 'Thoma',
-    rarity: 4
+    rarity: 4,
   },
   {
-    id: 'Collei',
-    level: 50,
-    rarity: 4,
-    uncap: 1
+    id: 'Venti',
+    level: 80,
+    rarity: 5,
   },
   {
-    id: 'Dori',
-    level: 50,
+    id: 'Xiangling',
+    level: 80,
     rarity: 4,
-    uncap: 1
+    uncap: 6,
   },
   {
-    id: 'Kaeya',
-    level: 50,
+    id: 'Xingqiu',
+    level: 80,
     rarity: 4,
-    uncap: 1
-  },
-  {
-    id: 'Lisa',
-    level: 50,
-    rarity: 4,
-    uncap: 2
-  },
-  {
-    id: 'Razor',
-    level: 50,
-    rarity: 4,
-    uncap: 6
-  },
-  {
-    id: 'Sayu',
-    level: 50,
-    rarity: 4,
-    uncap: 5
+    uncap: 6,
   },
   {
     id: 'Xinyan',
     level: 50,
     rarity: 4,
-    uncap: 3
+    uncap: 3,
+  },
+  {
+    id: 'Yaoyao',
+    level: 50,
+    rarity: 4,
+  },
+  {
+    id: 'Yelan',
+    level: 50,
+    rarity: 5,
   },
   {
     id: 'Yunjin',
     level: 50,
     name: 'Yun Jin',
-    rarity: 4
-  },
-  {
-    id: 'Yaoyao',
-    level: 50,
-    rarity: 4
-  },
-  {
-    id: 'Gorou',
-    level: 50,
-    rarity: 4
+    rarity: 4,
   },
 ];
