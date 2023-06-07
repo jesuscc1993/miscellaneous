79d5258b82a569b30d358d3b0781656d6f9f45a2
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2023-06-07 12:06:20 +0200

    Added jsonToProperties.

--- web-tools/controller.js
+++ web-tools/controller.js
@@ -192,6 +192,28 @@ const getPropertiesRecursivelySorted = (json) => {
 };
 
 // ==================================================================================================================
+// JSON to *.properties
+// ==================================================================================================================
+
+const jsonToPropertiesInput = jQuery('#jsonToPropertiesInput');
+const jsonToPropertiesOutput = jQuery('#jsonToPropertiesOutput');
+
+const jsonToProperties = () => {
+  const json = jsonToPropertiesInput.val();
+
+  const properties = replaceJsonWithProperties(json);
+  jsonToPropertiesOutput.val(properties);
+
+  replicateHeight(jsonToPropertiesInput, jsonToPropertiesOutput);
+};
+
+const replaceJsonWithProperties = (string) => {
+  return string
+    .replace(/(^{\s*|\s*}$)/g, '')
+    .replace(/"(.*?)": "(.*)",?/g, '$1=$2');
+};
+
+// ==================================================================================================================
 // Case transformer
 // ==================================================================================================================
 
