3ed7fa96d67ea6cb0683b7a0f1fc0c16010db195
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-08-12 19:07:25 +0200

    Refactored.

--- citra-layout-generator/index.js
+++ citra-layout-generator/index.js
@@ -3,36 +3,60 @@ const defaultTopHeight = 240;
 const defaultBottomWidth = 320;
 const defaultBottomHeight = 240;
 
-const generateLayout = () => {
-  const monitorWidth = parseInt(jQuery('#monitorWidth').val(), 10);
-  const monitorHeight = parseInt(jQuery('#monitorHeight').val(), 10);
-  // const topWidth = parseInt(jQuery('#topWidth').val(), 10);
-  const topHeight = parseInt(jQuery('#topHeight').val(), 10);
-  // const bottomWidth = parseInt(jQuery('#bottomWidth').val(), 10);
-  const bottomHeight = parseInt(jQuery('#bottomHeight').val(), 10);
+const monitorWidthEl = jQuery('#monitorWidth');
+const monitorHeightEl = jQuery('#monitorHeight');
+const topWidthEl = jQuery('#topWidth');
+const topHeightEl = jQuery('#topHeight');
+const bottomWidthEl = jQuery('#bottomWidth');
+const bottomHeightEl = jQuery('#bottomHeight');
 
-  if (!(monitorWidth && monitorHeight && topHeight && bottomHeight)) {
+const outputEl = jQuery('#output');
+const outputWrapperEl = jQuery('#output-wrapper');
+const previewEl = jQuery('#preview');
+const previewTopEl = previewEl.find('#top');
+const previewBottomEl = previewEl.find('#bottom');
+const copyOutputEl = previewEl.find('#copy-output');
+
+const generateLayout = () => {
+  const monitorWidth = parseInt(monitorWidthEl.val(), 10);
+  const monitorHeight = parseInt(monitorHeightEl.val(), 10);
+  const topWidth = parseInt(topWidthEl.val(), 10);
+  const topHeight = parseInt(topHeightEl.val(), 10);
+  const bottomWidth = parseInt(bottomWidthEl.val(), 10);
+  const bottomHeight = parseInt(bottomHeightEl.val(), 10);
+
+  if (
+    !(
+      bottomHeight &&
+      bottomWidth &&
+      monitorHeight &&
+      monitorWidth &&
+      topHeight &&
+      topWidth
+    )
+  ) {
     alert('Form is incomplete.');
     return;
   }
 
   generateVerticalLayout({
     bottomHeight,
+    bottomWidth,
     monitorHeight,
     monitorWidth,
     topHeight,
+    topWidth,
   });
 };
 
 const generateVerticalLayout = ({
   bottomHeight,
+  bottomWidth,
   monitorHeight,
   monitorWidth,
   topHeight,
+  topWidth,
 }) => {
-  const topWidth = (topHeight * defaultTopWidth) / defaultTopHeight;
-  const bottomWidth = (bottomHeight * defaultBottomWidth) / defaultBottomHeight;
-
   if (topWidth > monitorWidth || bottomWidth > monitorWidth) {
     alert('Screens are too wide to fit your monitor.');
     return;
@@ -52,24 +76,25 @@ const generateVerticalLayout = ({
   const bottomRight = (monitorWidth - bottomWidth) / 2 + bottomWidth;
   const bottomBottom = monitorHeight;
 
-  jQuery('#topWidth').val(topWidth);
-  jQuery('#bottomWidth').val(bottomWidth);
-
   outputLayout({
-    bottomBottom,
     bottomHeight,
+    bottomWidth,
+    monitorHeight,
+    monitorWidth,
+    topHeight,
+    topWidth,
+
+    bottomBottom,
     bottomLeft,
     bottomRight,
     bottomTop,
     bottomWidth,
-    monitorHeight,
-    monitorWidth,
+
     topBottom,
     topHeight,
     topLeft,
     topRight,
     topTop,
-    topWidth,
   });
 };
 
@@ -89,7 +114,7 @@ const outputLayout = ({
   topTop,
   topWidth,
 }) => {
-  jQuery('#output').text(
+  outputEl.text(
     `custom_layout\\default=false
 custom_layout=true
 custom_top_left\\default=false
@@ -110,28 +135,52 @@ custom_bottom_bottom\\default=false
 custom_bottom_bottom=${bottomBottom}`
   );
 
-  jQuery('#preview').css({ width: monitorWidth, height: monitorHeight });
+  previewEl.css({ width: monitorWidth, height: monitorHeight });
 
-  jQuery('#preview #top').css({
+  previewTopEl.css({
     width: topWidth,
     height: topHeight,
     top: topTop,
     left: topLeft,
   });
-  jQuery('#preview #bottom').css({
+  previewBottomEl.css({
     width: bottomWidth,
     height: bottomHeight,
     top: bottomTop,
     left: bottomLeft,
   });
 
-  jQuery('#output-wrapper').removeAttr('hidden');
-  jQuery('#copy').text('Copy layout.');
+  outputWrapperEl.removeAttr('hidden');
+  copyOutputEl.text('Copy layout.');
 
   copyLayout(true);
 };
 
 const copyLayout = (silent = false) => {
-  navigator.clipboard.writeText(jQuery('#output').text());
-  if (!silent) jQuery('#copy').text('Layout copied.');
+  navigator.clipboard.writeText(outputEl.text());
+  if (!silent) copyOutputEl.text('Layout copied.');
+};
+
+const recalculateTopHeight = () => {
+  topHeightEl.val(
+    Math.round((topWidthEl.val() / defaultTopWidth) * defaultTopHeight)
+  );
+};
+const recalculateBottomHeight = () => {
+  bottomHeightEl.val(
+    Math.round((bottomWidthEl.val() / defaultBottomWidth) * defaultBottomHeight)
+  );
+};
+
+const recalculateTopWidth = () => {
+  topWidthEl.val(
+    Math.round((topHeightEl.val() * defaultTopWidth) / defaultTopHeight)
+  );
+};
+const recalculateBottomWidth = () => {
+  bottomWidthEl.val(
+    Math.round(
+      (bottomHeightEl.val() * defaultBottomWidth) / defaultBottomHeight
+    )
+  );
 };
