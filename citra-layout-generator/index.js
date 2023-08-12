2635bcc283883bc70e87193861fb95c410e9c6d5
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-08-12 17:54:30 +0200

    Refactored.

--- citra-layout-generator/index.js
+++ citra-layout-generator/index.js
@@ -3,6 +3,27 @@ const defaultTopHeight = 240;
 const defaultBottomWidth = 320;
 const defaultBottomHeight = 240;
 
+const generateLayout = () => {
+  const monitorWidth = parseInt(jQuery('#monitorWidth').val(), 10);
+  const monitorHeight = parseInt(jQuery('#monitorHeight').val(), 10);
+  // const topWidth = parseInt(jQuery('#topWidth').val(), 10);
+  const topHeight = parseInt(jQuery('#topHeight').val(), 10);
+  // const bottomWidth = parseInt(jQuery('#bottomWidth').val(), 10);
+  const bottomHeight = parseInt(jQuery('#bottomHeight').val(), 10);
+
+  if (!(monitorWidth && monitorHeight && topHeight && bottomHeight)) {
+    alert('Form is incomplete.');
+    return;
+  }
+
+  generateVerticalLayout({
+    bottomHeight,
+    monitorHeight,
+    monitorWidth,
+    topHeight,
+  });
+};
+
 const generateVerticalLayout = ({
   bottomHeight,
   monitorHeight,
@@ -22,6 +43,24 @@ const generateVerticalLayout = ({
     return;
   }
 
+  outputLayout({
+    bottomHeight,
+    bottomWidth,
+    monitorHeight,
+    monitorWidth,
+    topHeight,
+    topWidth,
+  });
+};
+
+const outputLayout = ({
+  bottomHeight,
+  bottomWidth,
+  monitorHeight,
+  monitorWidth,
+  topHeight,
+  topWidth,
+}) => {
   jQuery('#output').text(
     `custom_layout\\default=false
 custom_layout=true
@@ -42,29 +81,9 @@ custom_bottom_right=${(monitorWidth - bottomWidth) / 2 + bottomWidth}
 custom_bottom_bottom\\default=false
 custom_bottom_bottom=${monitorHeight}`
   );
+
   jQuery('#output-wrapper').removeAttr('hidden');
   jQuery('#copy').text('Copy layout.');
-};
-
-const generateLayout = () => {
-  const monitorWidth = parseInt(jQuery('#monitorWidth').val(), 10);
-  const monitorHeight = parseInt(jQuery('#monitorHeight').val(), 10);
-  // const topWidth = parseInt(jQuery('#topWidth').val(), 10);
-  const topHeight = parseInt(jQuery('#topHeight').val(), 10);
-  // const bottomWidth = parseInt(jQuery('#bottomWidth').val(), 10);
-  const bottomHeight = parseInt(jQuery('#bottomHeight').val(), 10);
-
-  if (!(monitorWidth && monitorHeight && topHeight && bottomHeight)) {
-    alert('Form is incomplete.');
-    return;
-  }
-
-  generateVerticalLayout({
-    bottomHeight,
-    monitorHeight,
-    monitorWidth,
-    topHeight,
-  });
 };
 
 const copyLayout = () => {
