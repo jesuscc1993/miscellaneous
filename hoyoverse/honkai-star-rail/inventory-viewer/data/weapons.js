f70d2c25d9d249e783004947cae106ef07cce1af
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2024-01-20 14:22:12 +0100

    Updated styles.

--- hoyoverse/honkai-star-rail/inventory-viewer/data/weapons.js
+++ hoyoverse/honkai-star-rail/inventory-viewer/data/weapons.js
@@ -2,7 +2,7 @@ const weapons = [
   { name: 'Chorus',                       level: 60, rarity: 3, uncap: 5, type: 'Harmony',      portrait: 'u17ehb/9481816b3907d41d32bd1cae8133461b' },
   { name: 'Meshing Cogs',                 level: 70, rarity: 3, uncap: 5, type: 'Harmony',      portrait: 'ud64eb/21baf4cbb3fcc34fa849a9db9fa91750' },
   { name: 'Quid Pro Quo',                 level: 50, rarity: 4, uncap: 1, type: 'Abundance',    portrait: 'u17ehb/6b85f4dd55cd8b48ac1fe61dd81db280' },
-  { name: 'The Seriousness of Breakfast', level: 70, rarity: 4, uncap: 3, type: 'Erudition',    portrait: 'ud64eb/cab9103e1fd3e69158fa2735e80046a1', fullName: 'Trend of the Universal Market' },
+  { name: 'The Seriousness...',        level: 70, rarity: 4, uncap: 3, type: 'Erudition',    portrait: 'ud64eb/cab9103e1fd3e69158fa2735e80046a1', fullName: 'The Seriousness of Breakfast' },
   { name: 'Time Waits\nfor No One',       level: 70, rarity: 5, uncap: 1, type: 'Abundance',    portrait: 'ua57cb/69871065f148ed80081894d4426e1e16' },
   { name: 'Trend of the Universal...',    level: 70, rarity: 4, uncap: 2, type: 'Preservation', portrait: 'ud64eb/00d04115238823bf8fdaa90cc1143101', fullName: 'Trend of the Universal Market' },
 ];
