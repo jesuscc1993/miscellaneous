be160337e3a7f21e6a4ed9fef756b8006b09fdc2
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2024-12-31 16:08:10 +0100

    Improved inline sorting.

--- web-tools/constants/constants.js
+++ web-tools/constants/constants.js
@@ -1,3 +1,6 @@
-const INVALID_JSON_MESSAGE = '<Not a valid JSON object>';
 const LINE_DELIMITER = /\n/;
-const LIST_SEPARATOR = /,\s*/;
+const LIST_COMMA_SEPARATOR = /,\s*/;
+const LIST_SPACE_SEPARATOR = /\s+/;
+
+const COMMA = ',';
+const INVALID_JSON_MESSAGE = '<Not a valid JSON object>';
--- web-tools/controllers/sections/inline-list-sorter.js
+++ web-tools/controllers/sections/inline-list-sorter.js
@@ -13,20 +13,30 @@ const sortInlineList = () => {
   replicateHeight(unsortedInlineListInput, sortedInlineListOutput);
 };
 
-const getSortedInlineListLine = (line) => {
-  let array = line.split(LIST_SEPARATOR);
+const getSortedInlineListLine = (line, separator) => {
+  let array = line.split(separator);
 
   if (inlineListSorterDiscardDuplicates.checked) {
     array = getDuplicateFreeArray(array);
   }
 
-  return array.sort().join(', ');
+  return array
+    .sort()
+    .join(`${separator === LIST_COMMA_SEPARATOR ? COMMA : ''} `);
 };
 
 const getSortedInlineList = (inputString) => {
+  const separator = getSeparator(inputString);
+
   return inputString
-    .replace(new RegExp(` +(${LIST_SEPARATOR}) +`, 'g'), '$1')
+    .replace(new RegExp(` +(${separator}) +`, 'g'), '$1')
     .split(LINE_DELIMITER)
-    .map(getSortedInlineListLine)
+    .map((line) => getSortedInlineListLine(line, separator))
     .join(LINE_DELIMITER);
+};
+
+const getSeparator = (inputString) => {
+  return inputString.match(LIST_COMMA_SEPARATOR)
+    ? LIST_COMMA_SEPARATOR
+    : LIST_SPACE_SEPARATOR;
 };
