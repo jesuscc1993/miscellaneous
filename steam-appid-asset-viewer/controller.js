a54d2b03a8e8a1d5c20024ff3b5791c1bde82d5e
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-03-02 03:21:18 +0100

    Added more links.

--- steam-appid-asset-viewer/controller.js
+++ steam-appid-asset-viewer/controller.js
@@ -1,10 +1,23 @@
-function updateImages(event) {
+const storeAnchor = document.getElementById('storeAnchor');
+const assetImgAnchors = document.querySelectorAll('.asset-group a');
+const assetImgs = document.querySelectorAll('.asset-group a img');
+
+const updateImages = (event) => {
   event?.preventDefault();
   event?.stopPropagation();
 
   const appId = parseInt(document.getElementById('appIdInput').value, 10);
 
-  document.querySelectorAll('img').forEach((img) => {
-    img.src = img.src.replace(/\/\d+\//, `\/${appId}\/`);
+  assetImgAnchors.forEach((anchor) => {
+    anchor.href = getUpdatedAppIdUrl(appId, anchor.href);
   });
-}
+  assetImgs.forEach((img) => {
+    img.src = getUpdatedAppIdUrl(appId, img.src);
+  });
+
+  storeAnchor.href = storeAnchor.href.replace(/\/\d+/, `\/${appId}`);
+};
+
+const getUpdatedAppIdUrl = (appId, url) => {
+  return url.replace(/\/\d+\//, `/${appId}/`);
+};
